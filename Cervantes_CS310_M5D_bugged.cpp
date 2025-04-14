f2a89f838997959b09b1d2f32cd222e0a9a8293a
Norm Cervantes <normancervantes701@gmail.com>
2025-04-13 22:09:21 -0500

    Update Cervantes_CS310_M5D_bugged: Replaced placeholder with broken version for M5D

--- Cervantes_CS310_M5D_bugged.cpp
+++ Cervantes_CS310_M5D_bugged.cpp
@@ -1,20 +1,36 @@
-// Cervantes_CS310_M5D_bugged.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+int square(int value)
+{
+    result = value * value
+        return result;
+}
+
+bool isEven(int num)
+{
+    if num % 2 == 0
+        return true;
+    else
+        return false;
+}
+
+void displayResult(int number)
+{
+    std::cout << "The square of " << number << " is " << square(number) << std::endl
+        if (isEven(number))
+            std::cout << "The number is even." << std::endl;
+        else
+            std::cout << "The number is odd." << std::endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int userNumber
+
+        std::cout << "Enter a number: ";
+    std::cin >> usernumber;
+
+    displayResult(usernumber);
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
